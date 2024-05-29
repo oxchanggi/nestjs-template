5900decd191b2bd2be506936bb2d49d916d193c6
nierdna <cassie382.618@gmail.com>
2024-05-29 17:34:57 +0700

    update

--- src/modules/telegram-bot/ui/pages/main.page.ts
+++ src/modules/telegram-bot/ui/pages/main.page.ts
@@ -1,10 +1,8 @@
 import { EWalletType, buildPhotoOptions } from '@/telegram-bot/constants';
 import { PhotoResponse } from '@/telegram-bot/types';
 import { createMenuLabel, insertNewlines } from '@/telegram-bot/utils';
-import * as fs from 'fs';
-import { ButtonText } from 'lang/soybot/en/button';
-import path from 'path';
 import { PhotoPage } from './page';
+import { ButtonText } from 'lang/config/en/button';
 
 export class MainPage implements PhotoPage {
   constructor(
@@ -25,16 +23,13 @@ export class MainPage implements PhotoPage {
 
     const joinedWallet = insertNewlines(mapBalances, 1).join('');
 
-    const text = `Welcome to <b>SoyBot</b>, ${this.firstName}\n<b>The Fastest Sniper Bot on Solana.\nPowered by <a>$SOY</a></b>\n\nThis is your newly created SoyBot wallet.\nFund SOL and start trading!\n\nYou can explore more at <a href="https://t.me/verifySoybot"><b>SoyBot community</b></a>\n\n<b>=== Your Solana Wallets ===</b>\n${joinedWallet}`;
-
-    const imageFilePath = path.join(__dirname, '../../../../images/soybot.jpg');
-    const photo = fs.readFileSync(imageFilePath);
+    const text = `Welcome to <b>Bot</b>, ${this.firstName}\n<b>The Fastest Sniper Bot on Solana.\nPowered by <a>$SOY</a></b>\n\nThis is your newly created Bot wallet.\nFund SOL and start trading!\n\nYou can explore more at <a href="https://t.me/verifySoybot"><b>Bot community</b></a>\n\n<b>=== Your Solana Wallets ===</b>\n${joinedWallet}`;
 
     const menu = buildPhotoOptions(
       [[createMenuLabel(ButtonText.main.title)]],
       text,
     );
 
-    return { photo, menu };
+    return { photo: null, menu };
   }
 }
