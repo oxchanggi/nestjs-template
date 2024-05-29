ffe9bff8547d0694a298f48c22dcb1508fdcaf8a
nierdna <cassie382.618@gmail.com>
2024-05-29 19:05:15 +0700

    update

--- src/modules/worker/schedulers/schedule.service.ts
+++ src/modules/worker/schedulers/schedule.service.ts
@@ -1,12 +1,17 @@
 import { Cron } from '@nestjs/schedule';
-import { Injectable, OnApplicationBootstrap } from '@nestjs/common';
+import { Inject, Injectable, OnApplicationBootstrap } from '@nestjs/common';
+import { QueueService } from '@/queue/services';
 
 @Injectable()
 export class ScheduleService implements OnApplicationBootstrap {
+  @Inject(QueueService)
+  private readonly queueService: QueueService;
+
   async onApplicationBootstrap() {}
 
-  @Cron('*/1 * * * * *')
+  @Cron('*/10 * * * * *')
   async handleCron() {
-    console.log('Called when the current second is 1');
+    console.log('Call queue service');
+    await this.queueService.addJob({ test: 'test' });
   }
 }
--- src/modules/worker/worker.module.ts
+++ src/modules/worker/worker.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from '@/database';
 import { ScheduleService } from './schedulers/schedule.service';
 import { ScheduleModule } from '@nestjs/schedule';
+import { QueueModule } from '@/queue';
 
 const isWorker = Boolean(Number(process.env.IS_WORKER || 0));
 
@@ -11,7 +12,7 @@ if (isWorker) {
 }
 
 @Module({
-  imports: [DatabaseModule, ScheduleModule.forRoot()],
+  imports: [DatabaseModule, ScheduleModule.forRoot(), QueueModule],
   controllers: [],
   providers: [...schedulers],
   exports: [],
